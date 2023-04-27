fbffe73f8a28e9a2f3cc5b77456aa813f8b5ca20
Jose Jereza <92663129+jvjereza@users.noreply.github.com>
2023-04-27 20:38:18 +0800

    Update ProductionTeamPage.jsx

--- react/src/ClientPage/ProductionTeamPage.jsx
+++ react/src/ClientPage/ProductionTeamPage.jsx
@@ -38,52 +38,51 @@ function ProductionTeamPage() {
   ]);
 
   return (
-    <div id="features" className="offset">
+    <><div id="features" className="offset d-flex justify-content-center">
+      <Container fluid>
+        <h3 className="text-center mb-4">Our Works</h3>
+        <hr className="heading-underline" />
+        <Row>
+          {features.map((feature) => (
+            <Col md="4" key={feature.id}>
+              <div className="feature">
+                <a href="#">
+                  <img src={feature.imgSrc} alt={feature.altText} />
+                </a>
+                <p>{feature.description}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div><div id="team" className="offset d-flex justify-content-center">
         <Container fluid>
-          <h3 className="text-center mb-4">Our Works</h3>
+          <h1 className="text-center mb-4">Our Production Team</h1>
           <hr className="heading-underline" />
-          <Row className="text-center">
-            {features.map((feature) => (
-              <Col md="4" key={feature.id}>
-                <div className="feature">
+          <Row>
+            {teamMembers.map((member) => (
+              <Col md="4" key={member.id}>
+                <div className="team">
                   <a href="#">
-                    <img src={feature.imgSrc} alt={feature.altText} />
+                    <img src={member.imgSrc} alt={member.altText} />
                   </a>
-                  <p>{feature.description}</p>
                 </div>
               </Col>
             ))}
+            <Col md="4">
+              <div className="feature">
+                <p>
+                  I wanted to introduce you to our team who has an extensive background in modular cabinet production, and they will bring their experience and skills to our projects. They have a passion for the production and intend to adopt a more sustainable approach to our marketing in the future. I am sure you will all work with them to ensure a wonderful experience for our company.
+                  <br />
+                  <br />
+                  Cheers.
+                </p>
+              </div>
+            </Col>
           </Row>
         </Container>
-      
-      <div id="team" className="offset">
-          <Container fluid>
-            <h1 className="text-center mb-4">Our Production Team</h1>
-            <hr className="heading-underline" />
-            <Row className="text-center">
-              {teamMembers.map((member) => (
-                <Col md="4" key={member.id}>
-                  <div className="team">
-                    <a href="#">
-                      <img src={member.imgSrc} alt={member.altText} />
-                    </a>
-                  </div>
-                </Col>
-              ))}
-              <Col md="4">
-                <div className="feature">
-                  <p>
-                    I wanted to introduce you to our team who has an extensive background in modular cabinet production, and they will bring their experience and skills to our projects. They have a passion for the production and intend to adopt a more sustainable approach to our marketing in the future. I am sure you will all work with them to ensure a wonderful experience for our company.
-                    <br />
-                    <br />
-                    Cheers.
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-      </div>
-    </div>
+      </div></>
+
   );
 }
 
